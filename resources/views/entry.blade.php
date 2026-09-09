d31c9d0c03df8ef7ebcec15d18005ac3ed49ea74
Neo83damian <148293408+Neo83damian@users.noreply.github.com>
2026-09-09 08:13:21 +0800

    Update entry.blade.php

--- resources/views/entry.blade.php
+++ resources/views/entry.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=0.7">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>REDFLOW - Blood Types Digital Masterlist</title>
 
